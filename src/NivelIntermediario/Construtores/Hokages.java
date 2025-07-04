362af93c746e65752834d32e0ed242e32167db52
Gustavo Zambeli <zambelidev@gmail.com>
2025-07-04 17:42:07 -0300

    Finalizando Aula de `Construtores`

---
+++ src/NivelIntermediario/Construtores/Hokages.java
@@ -0,0 +1,25 @@
+package NivelIntermediario.Construtores;
+
+public class Hokages {
+
+    String nome;
+    int idade;
+    boolean vivoOuNao;
+    String aldeia;
+    int missoes;
+    double saldoBancario;
+    double altura;
+
+    public Hokages() {
+    }
+
+    public Hokages(String nome, int idade, boolean vivoOuNao, String aldeia, int missoes, double saldoBancario, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.vivoOuNao = vivoOuNao;
+        this.aldeia = aldeia;
+        this.missoes = missoes;
+        this.saldoBancario = saldoBancario;
+        this.altura = altura;
+    }
+}
